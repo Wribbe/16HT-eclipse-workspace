aec400e819ee968c8da49b707984d43423c0327d
wribbe <eng_steff@hotmail.com>
2016-09-17 11:48:01 +0200

    Add static debug() function for debug printout.

--- exercise 1/src/buffer/Buffer.java
+++ exercise 1/src/buffer/Buffer.java
@@ -11,6 +11,7 @@ class Buffer {
 	Semaphore avail; // For blocking when no data is available.
 
 	final static int bufferSize = 8;
+	final static boolean DEBUG = true;
 	int currentIndex = 0;
 	String[] buffData; // The actual buffer.
 
@@ -26,37 +27,45 @@ class Buffer {
 		currentIndex = (currentIndex+1) % bufferSize;
 		return current;
 	}
-
-	void putLine(String input) {
-		System.out.println("Waitning for free.");
-		free.take(); // Wait for buffer empty.
-		System.out.println("Free taken.");
-		System.out.println("Waitning for mutex.");
-		mutex.take(); // Wait for exclusive access.
-		System.out.println("Mutex taken.");
-		System.out.println("New buffer.");
-		buffData[nextPos()] = new String(input);
-		System.out.println("Getting rid of mutex.");
-		mutex.give(); // Allow others to access.
-		System.out.println("Mutex gone.");
-		System.out.println("Getting rid of avail.");
-		avail.give(); // Allow others to get line.
-		System.out.println("Avail gone.");
+	
+	private static void debug(String message) {
+		if (DEBUG) {
+			System.out.println("[?] DEBUG: "+message);
+		}
 	}
 
-	String getLine() {
+	void putLine(String input) { // Used by producer.
+		debug("putLine waiting for free.");
+		free.take(); // Wait for buffer empty.
+		debug("putLine has free.");
+		debug("putLine waiting for mutex.");
+		mutex.take(); // Wait for exclusive access.
+		debug("putLine has mutex.");
+		debug("Got free in putLine.");
+		buffData[nextPos()] = new String(input);
+		mutex.give(); // Allow others to access.
+		debug("putLine has returned mutex.");
+		avail.give(); // Allow others to get line.
+		debug("putLine has returned avail.");
+	}
+
+	String getLine() { // Used by consumer.
 		// Exercise 2 ...
 		// Here you should add code so that if the buffer is empty, the
 		// calling process is delayed until a line becomes available.
 		// A caller of putLine hanging on buffer full should be released.
 		// ...
+		debug("getLine() is waiting for avail.");
 		avail.take(); // Wait for data.
+		debug("getLine() has got avail.");
+		debug("getLine() is waiting for mutex.");
 		mutex.take(); // Mutual exclusion.
+		debug("getLine() has got mutex.");
 		String ans = buffData[currentIndex]; // Get the data.
 		buffData[currentIndex] = null; // Reset buffer element.
 		nextPos(); // Advance position in buffer.
 		mutex.give(); // Return mutual exclusion.
-		free.give();  // Signal tkat the buffer is free.
+		free.give();  // Signal that the buffer is free.
 		return ans;
 	}
 }
