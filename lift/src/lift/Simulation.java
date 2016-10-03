9418ceb6818c882e4d093355a35769a642c87878
Stefan Eng <eng_steff@hotmail.com>
2016-10-03 15:44:15 +0200

    Add private random functions.

--- lift/src/lift/Simulation.java
+++ lift/src/lift/Simulation.java
@@ -7,8 +7,23 @@ public class Simulation {
 	Simulation() {
 		view = new LiftView();
 	}
+	
+	private float randomFloat(float max) {
+		return (float)Math.random()*(max+1);
+	}
+
+	private float randomFloat(int max) {
+		return randomFloat((float)max);
+	}
+	
+	private int randomDelay(int maxMill) {
+		return (int)(1000*randomFloat(maxMill));
+	}
 
 	public static void main(String[] args) {
 		Simulation sim = new Simulation();
+		for(int i=0; i<20; i++) {
+			System.out.println(sim.randomDelay(10));
+		}
 	}
 }
