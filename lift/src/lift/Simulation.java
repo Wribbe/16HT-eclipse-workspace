a2d86ca2781458c2deac9c3d78024e53694cb942
Stefan Eng <eng_steff@hotmail.com>
2016-10-03 15:31:33 +0200

    Add simulation stub.

---
+++ lift/src/lift/Simulation.java
@@ -0,0 +1,14 @@
+package lift;
+
+public class Simulation {
+	
+	private LiftView view;
+	
+	Simulation() {
+		view = new LiftView();
+	}
+
+	public static void main(String[] args) {
+		Simulation sim = new Simulation();
+	}
+}
